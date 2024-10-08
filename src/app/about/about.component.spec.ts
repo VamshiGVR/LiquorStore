9a2cd95e251b949476c0b1f03dd712e94c9875ec
vamshi <vamshidharreddygundlapally@gmail.com>
2024-10-08 16:47:28 -0400

    test

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -21,3 +21,7 @@ describe('AboutComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+function beforeEach(arg0: () => Promise<void>) {
+  throw new Error('Function not implemented.');
+}
+
