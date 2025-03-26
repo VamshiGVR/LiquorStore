3a2a1c94b115f2af69110ceacbba602425998460
Vamshidhar Reddy Gundlapally <163581641+VamshiGVR@users.noreply.github.com>
2025-03-26 10:23:40 -0400

    Added configurations between frontend and backend.

--- LiquorStoreBE/src/main/java/com/liquor_store/project/Controller/LiquorController.java
+++ LiquorStoreBE/src/main/java/com/liquor_store/project/Controller/LiquorController.java
@@ -2,6 +2,7 @@ package com.liquor_store.project.Controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.liquor_store.project.Service.LiquorService;
 
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/LiquorStore")
 public class LiquorController {
 	
--- LiquorStoreFE/src/app/app.component.ts
+++ LiquorStoreFE/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './services/api.service';
 import { MainPageComponent } from "./main-page/main-page.component";
 
 @Component({
@@ -8,6 +8,15 @@ import { MainPageComponent } from "./main-page/main-page.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'liquor-store';
+export class AppComponent implements OnInit {
+  message: string = '';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getHelloMessage().subscribe(
+      (data) => this.message = data,
+      (error) => console.error('Error:', error)
+    );
+  }
 }
